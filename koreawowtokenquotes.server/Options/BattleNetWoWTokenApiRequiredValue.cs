703f8cf84247b6f6508b9760c8a960b9465dc3f1
Vincent <christian289@naver.com>
2025-01-14 17:23:29 +0900

    appsettings development 제거

--- koreawowtokenquotes.server/Options/BattleNetWoWTokenApiRequiredValue.cs
+++ koreawowtokenquotes.server/Options/BattleNetWoWTokenApiRequiredValue.cs
@@ -1,6 +1,6 @@
 namespace koreawowtokenquotes.server.Options;
 
-public class BattleNetRequiredValue
+public class BattleNetWoWTokenApiRequiredValue
 {
     public required string Region { get; set; }
     public required string Namespace { get; set; }
